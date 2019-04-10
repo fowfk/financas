433866116ab184b16c49bfeca1b0a01f56b71bd9
Bruno Campos <brunocamposfow@gmail.com>
2019-04-10 16:36:39 -0300

    Estados do JPA

--- financas/src/br/com/caelum/financas/teste/TesteConta.java
+++ financas/src/br/com/caelum/financas/teste/TesteConta.java
@@ -1,32 +1,32 @@
 package br.com.caelum.financas.teste;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
 
 public class TesteConta {
 	
 	public static void main(String[] args) {
 		
 		Conta conta = new Conta();
+		conta.setId(1);
 		conta.setTitular("Bruno");
 		conta.setAgencia("1798");
 		conta.setBanco("Inter");
 		conta.setNumero("0006568");
 		
 		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
-		EntityManager em = emf.createEntityManager();
+		EntityManager em = new JPAUtil().getEntityManager();
 		
 		
 		em.getTransaction().begin();
-		em.persist(conta);
+		conta = em.find(Conta.class, 1);
+		
+		em.remove(conta);
 		em.getTransaction().commit();
 		
 		em.close();
-		emf.close();
 		
 	}
 
